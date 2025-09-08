305de8536996e639d3923612a87a9ff5c710861d
Jade R-L <119926041+jader01@users.noreply.github.com>
2025-09-08 09:28:50 +0200

    Update bdd.db.sql

--- bdd.db.sql
+++ bdd.db.sql
@@ -1,37 +1,32 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "client" (
-	"id_client"	INTEGER UNIQUE,
-	"nom"	TEXT NOT NULL,
-	"ticket"	TEXT,
+CREATE TABLE IF NOT EXISTS "membres" (
+	"Nom"	TEXT NOT NULL,
+	"id"	INTEGER,
 	"entreprise"	TEXT,
-	PRIMARY KEY("id_client" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "developpeur" (
-	"id_dev"	INTEGER,
-	"nom"	TEXT NOT NULL,
-	"ticket"	TEXT,
-	PRIMARY KEY("id_dev" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "rapporteur" (
-	"id_rapporteur"	INTEGER NOT NULL,
-	"nom"	TEXT NOT NULL,
-	"ticket"	TEXT,
-	PRIMARY KEY("id_rapporteur" AUTOINCREMENT)
+	"role"	TEXT NOT NULL,
+	"TOC"	TEXT
 );
 CREATE TABLE IF NOT EXISTS "ticket" (
 	"id_ticket"	INTEGER,
-	"titre"	TEXT NOT NULL,
+	"stitre"	TEXT,
 	"description"	TEXT,
 	"status"	TEXT,
 	"priorite"	TEXT,
 	"id_rapporteur"	INTEGER,
-	"id_clev"	INTEGER,
-	"id_client"	INTEGER,
-	FOREIGN KEY("id_clev") REFERENCES "developpeur"("id_dev"),
-	FOREIGN KEY("id_rapporteur") REFERENCES "rapporteur"("id_rapporteur")
+	"id_developpeur"	INTEGER,
+	"id_client"	INTEGER
 );
-INSERT INTO "client" VALUES (1,'alex','TOC1','sdis');
-INSERT INTO "developpeur" VALUES (1,'thomas','TOC1');
-INSERT INTO "rapporteur" VALUES (1,'jade','TOC1');
-INSERT INTO "ticket" VALUES (1,'ROS','on nous signale un défaut ROS secteur 1 LTE 800','ouvert','P3',1,1,1);
+INSERT INTO "membres" ("Nom","id","entreprise","role","TOC") VALUES ('Thomas',1,'Axians','devellopeur','TOC1'),
+ ('Jade',2,'Axians','rapporteur','TOC1'),
+ ('Alex',3,'Sdis','client','TOC2'),
+ ('Thomas',1,'Axians','devellopeur','TOC2'),
+ ('Thomas',1,'Axians','devellopeur','TOC3'),
+ ('Alex',3,'Sdis','client','TOC3'),
+ ('Alex',3,'Sdis','client','TOC1'),
+ ('Jade',2,'Axians','rapporteur','TOC2'),
+ ('Jade',2,'Axians','rapporteur','TOC3');
+INSERT INTO "ticket" ("id_ticket","titre","description","status","priorite","id_rapporteur","id_developpeur","id_client") VALUES ('TOC1','ROS','on nous signale un défaut ROS sur le secteur 2 LTE 800','ouvert','P4',2,1,3),
+ ('TOC2','EVT','defaut extracteur','ouvert','P3',2,1,3),
+ ('TOC3','RSSI','on nous signale un defaut d''interference au niveau du S3','ouvert','P2',2,1,3);
+CREATE VIEW "test" AS SELECT * FROM "main"."membres" WHERE "Nom" LIKE '%Thomas%' ESCAPE '\' AND "TOC" LIKE '%TOC1%' ESCAPE '\' AND "entreprise" LIKE '%Axians%' ESCAPE '\' AND "id" LIKE '%1%' ESCAPE '\' AND "role" LIKE '%developpeur%' ESCAPE '\' ORDER BY "role" ASC;
 COMMIT;
